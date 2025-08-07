2c6355cea36ad97f952381cbb651721506348846
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-08-07 16:27:37 -0300

    salario

---
+++ Heranca/salario/Empregado.java
@@ -0,0 +1,24 @@
+package salario;
+
+public class Empregado {
+	private double salarioFixo;
+
+	public Empregado(double salarioFixo) {
+		super();
+		this.salarioFixo = salarioFixo;
+	}
+
+	public double getSalarioFixo() {
+		return salarioFixo;
+	}
+
+	public double calcularBonus(Departamento departamento) {
+		if (departamento.alcancouMeta())
+			return this.salarioFixo * 0.1;
+		return 0;
+	}
+
+	public double calcularSalarioTotal(Departamento departamento) {
+		return this.salarioFixo + calcularBonus(departamento);
+	}
+}
---
+++ Heranca/salario/Gerente.java
@@ -0,0 +1,15 @@
+package salario;
+
+public class Gerente extends Empregado {
+
+	public Gerente(double salarioFixo) {
+		super(salarioFixo);
+		// TODO Auto-generated constructor stub
+	}
+
+	public double calcularBonus(Departamento departamento) {
+		if (departamento.alcancouMeta())
+			return getSalarioFixo() * 0.2;
+		return 0;
+	}
+}
