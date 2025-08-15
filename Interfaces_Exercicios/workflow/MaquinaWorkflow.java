6a7063819db381d34a8183c3c60b5d60c7d8702a
Guilherme Gomes <guilherme.gomes.orgm@gmail.com>
2025-08-15 07:52:22 -0300

    interface exercicios 1

---
+++ Interfaces_Exercicios/workflow/MaquinaWorkflow.java
@@ -0,0 +1,12 @@
+import atividades.Atividade;
+
+public class MaquinaWorkflow {
+    public MaquinaWorkflow() {
+    }
+
+    public void executar(Workflow workflow) {
+        for (Atividade atividade : workflow.atividades) {
+            atividade.executar();
+        }
+    }
+}
