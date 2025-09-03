8e54f25bcc929fb05fdbed75573e2801d0d74edc
Guilherme Gomes <guilherme.gomes.orgm@gmail.com>
2025-09-03 19:01:01 -0300

    generics armazem especifico

---
+++ Generics_Exercises/armazem_especifico/Eletronico.java
@@ -0,0 +1,15 @@
+
+public class Eletronico {
+    private String descricao;
+    private double valor;
+
+    public Eletronico(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.descricao + "] R$ " + String.format("%.6f", this.valor);
+    }
+}
