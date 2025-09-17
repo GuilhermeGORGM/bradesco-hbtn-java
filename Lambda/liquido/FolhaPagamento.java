0d0623147401f0e5df531b5180d8256ca3b97f1c
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-09-17 14:22:20 -0300

    Lambda liquido

---
+++ Lambda/liquido/FolhaPagamento.java
@@ -0,0 +1,6 @@
+
+public class FolhaPagamento {
+	static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculador) {
+		return calculador.calcular(funcionario.getSalario());
+	}
+}
