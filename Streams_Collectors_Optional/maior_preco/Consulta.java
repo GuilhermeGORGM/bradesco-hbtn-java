f18aff997ef85d1296953dcb49de403fa3d8e049
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-09-19 17:34:34 -0300

    streams 2

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,13 @@
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Consulta {
+	static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+		return pedido.getProdutos().stream().filter(p -> p.getCategoria().equals(CategoriaProduto.LIVRO)).toList();
+	}
+
+	static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+		return produtos.stream().max(Comparator.comparingDouble(Produto::getPreco)).get();
+	}
+}
