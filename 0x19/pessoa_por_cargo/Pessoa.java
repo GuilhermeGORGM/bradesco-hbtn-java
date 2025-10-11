753a8f383b95893f64b51c536cf3ce7b7ea340a4
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-10-11 17:09:22 -0300

    Collector cargo

---
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -0,0 +1,26 @@
+
+public class Pessoa {
+	private int codigo;
+	private String nome;
+	private String cargo;
+	private int idade;
+	private double salario;
+
+	public String toString() {
+		return "[" + codigo + "] " + nome + " " + cargo + " " + idade + " R$ " + String.format("%,6f", salario);
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.cargo = cargo;
+		this.idade = idade;
+		this.salario = salario;
+	}
+
+}
