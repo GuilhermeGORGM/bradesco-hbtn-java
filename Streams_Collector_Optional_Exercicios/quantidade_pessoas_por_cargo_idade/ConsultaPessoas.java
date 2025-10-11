555fac8aab59a4efea2599808fe355a865f12e36
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-10-11 18:54:08 -0300

    Collector stream opt 2

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade/ConsultaPessoas.java
@@ -0,0 +1,12 @@
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+	public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas) {
+		Map<String, Map<Integer, Long>> pessoasPorCargo = pessoas.stream()
+				.collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())));
+		return pessoasPorCargo;
+	}
+}
---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade/Pessoa.java
@@ -0,0 +1,36 @@
+
+public class Pessoa implements Comparable<Pessoa> {
+	private int codigo;
+	private String nome;
+	private String cargo;
+	private int idade;
+	private double salario;
+
+	@Override
+	public String toString() {
+		return "[" + codigo + "] " + nome + " " + cargo + " " + idade + " R$ "
+				+ String.format("%.6f", salario).replace('.', ',');
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.cargo = cargo;
+		this.idade = idade;
+		this.salario = salario;
+	}
+
+	@Override
+	public int compareTo(Pessoa o) {
+		return this.nome.compareTo(o.nome);
+	}
+}
