4495e17ef3823ee6764f8fa9a36ae7c13fa9566b
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-10-11 17:37:43 -0300

    Collector idade

---
+++ 0x19/idade_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,17 @@
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+	public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+		Map<String, List<Pessoa>> pessoasPorCargo = pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo));
+		return pessoasPorCargo;
+	}
+
+	public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+		Map<String, List<Integer>> idadePorCargo = pessoas.stream().collect(
+				Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+		return idadePorCargo;
+	}
+}
