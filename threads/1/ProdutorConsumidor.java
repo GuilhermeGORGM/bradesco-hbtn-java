d7a7c8b0d4bfeb75dc5e1743816ba37115a28dd2
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-10-14 15:22:44 -0300

    Threads 1

---
+++ threads/1/ProdutorConsumidor.java
@@ -0,0 +1,13 @@
+
+public class ProdutorConsumidor {
+	public static void main(String[] args) {
+		Buffer buffer = new Buffer();
+
+		// Criar e iniciar as threads do produtor e consumidor
+		Produtor producer = new Produtor(buffer);
+		Consumidor consumer = new Consumidor(buffer);
+
+		producer.start();
+		consumer.start();
+	}
+}
