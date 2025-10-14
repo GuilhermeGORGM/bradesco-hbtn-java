8c8b063036562272d2cfc8f85811652981314f58
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-10-14 15:10:42 -0300

    Threads 0

--- threads/0/Principal.java
+++ threads/0/Principal.java
@@ -6,8 +6,16 @@ public class Principal {
 		ThreadContador thread1 = new ThreadContador(count);
 		ThreadContador thread2 = new ThreadContador(count);
 
-		thread1.run();
-		thread2.run();
+		thread1.start();
+		thread2.start();
+
+		try {
+			thread1.join();
+			thread2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		System.out.println("Valor final do contador: " + count.getContagem());
 	}
