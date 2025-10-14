4bc1102a8fb9be4a489539960c467f4b0f499dca
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-10-14 15:24:38 -0300

    Threads 1

--- threads/1/Buffer.java
+++ threads/1/Buffer.java
@@ -1,10 +1,10 @@
 
 public class Buffer {
-	private int item = -1; // -1 indica que o buffer está vazio
+	private int item = -1; // -1 indica que o buffer esta vazio
 
-	// Método para o produtor colocar um item no buffer
+	// Metodo para o produtor colocar um item no buffer
 	public synchronized void produzir(int item) throws InterruptedException {
-		// Se o buffer já tem um item, espera o consumidor consumi-lo
+		// Se o buffer ja tem um item, espera o consumidor consumi-lo
 		while (this.item != -1) {
 			wait(); // Espera o consumidor consumir o item
 		}
@@ -17,7 +17,7 @@ public class Buffer {
 		notify();
 	}
 
-	// Método para o consumidor retirar um item do buffer
+	// Metodo para o consumidor retirar um item do buffer
 	public synchronized int consumir() throws InterruptedException {
 		// Se o buffer estiver vazio, espera o produtor produzir um item
 		while (this.item == -1) {
@@ -26,10 +26,10 @@ public class Buffer {
 
 		// Retira o item do buffer
 		int consumido = this.item;
-		this.item = -1; // Indica que o buffer está vazio
+		this.item = -1; // Indica que o buffer esta vazio
 		System.out.println("Consumidor consumiu: " + consumido);
 
-		// Notifica o produtor que o buffer está vazio
+		// Notifica o produtor que o buffer esta vazio
 		notify();
 
 		return consumido;
