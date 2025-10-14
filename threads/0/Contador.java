bccd31c34ecff728c2416c1c222896b73c2ee8bf
Guilherme <guilherme.gomes.orgm@gmail.com>
2025-10-14 15:12:11 -0300

    Threads 0

--- threads/0/Contador.java
+++ threads/0/Contador.java
@@ -2,7 +2,7 @@
 public class Contador {
 	private int count = 0;
 
-	// Método sincronizado para garantir que o contador seja atualizado de maneira
+	// Metodo sincronizado para garantir que o contador seja atualizado de maneira
 	// segura
 	public synchronized void incrementar() {
 		count++;
